cda491de9b65ec0ee9e846c0e197a1f94e1680e6
Oscar <odmarulandab@unal.edu.co>
2024-08-15 21:22:20 -0500

    funcion js eencriptar funcionando

--- app.js
+++ app.js
@@ -1,4 +1,7 @@
 function encriptar(){
     let original = document.getElementById("entrada").value;
-    alert("it works");
-}
+    let mod1 = original.replaceAll('a', 1).replaceAll('e', 2).replaceAll('i', 3).replaceAll('o', 4).replaceAll('u', 5);
+    let mod2 = mod1.replaceAll('1', 'ai').replaceAll('2', "enter").replaceAll('3', "imes").replaceAll('4', "ober").replaceAll('5', 'ufat');
+    resultado = document.getElementById("resultado");
+    resultado.innerHTML = mod2
+}   
